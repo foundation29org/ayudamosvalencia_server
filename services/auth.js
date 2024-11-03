cd1d7bc373ffe1db5a7d27fafb58bd80535a0e0b
Javier <javierlogronoaguirre@gmail.com>
2024-11-03 16:40:33 +0100

    fixed

--- services/auth.js
+++ services/auth.js
@@ -28,7 +28,7 @@ function decodeToken(token, roles){
 			const payload = jwt.decode(token, config.SECRET_TOKEN)
 			if(roles.includes(payload.role)){
 				let userId= crypt.decrypt(payload.sub);
-				await User.findById(userId, {"password" : false, "__v" : false, "confirmationCode" : false, "loginAttempts" : false, "confirmed" : false, "lastLogin" : false}, (err, user) => {
+				await User.findById(userId, {"password" : false, "__v" : false, "confirmationCode" : false, "loginAttempts" : false, "lastLogin" : false}, (err, user) => {
 					if(err){
 						reject({
 							status: 403,
@@ -49,7 +49,6 @@ function decodeToken(token, roles){
 									message: 'Token expired'
 								})
 							}
-
 							if (!user.confirmed) {
                                 reject({
                                     status: 403,
